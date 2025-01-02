8ad053bebe482081f5e8b91b738fd40a7f8cb01c
HimanPro <hp1950g@gmail.com>
2025-01-02 16:23:17 +0530

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -267,7 +267,7 @@ const Home = () => {
             <div className="flex flex-wrap justify-center">
 
 
-                <button className="flex items-center justify-center px-4 py-2 mx-1 my-2 text-gray-700 transition-colors duration-300 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+                <button onClick={handlePrevious} className="flex items-center justify-center px-4 py-2 mx-1 my-2 text-gray-700 transition-colors duration-300 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
                     </svg>
@@ -278,7 +278,7 @@ const Home = () => {
                     </a>
                 })}
 
-                <button className="flex items-center justify-center px-4 py-2 mx-1 my-2 text-gray-700 transition-colors duration-300 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+                <button onClick={handleNext} className="flex items-center justify-center px-4 py-2 mx-1 my-2 text-gray-700 transition-colors duration-300 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
                     </svg>
